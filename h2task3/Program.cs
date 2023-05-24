82f29c07869b454f8f14308c06be8484896a99fc
Admin <stas.revutsky@gmail.com>
2023-05-24 23:33:23 +0300

    Add project files.

---
+++ h2task3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace h2task3
+{
+    internal class Program
+    {
+        enum HTTPError 
+        {
+            BadRequest = 400,
+            Unauthorised,
+            PaymentRequired,
+            Forbidden,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter error code");
+            Console.WriteLine((HTTPError)Convert.ToInt32(Console.ReadLine()));
+        }   
+    }
+}
